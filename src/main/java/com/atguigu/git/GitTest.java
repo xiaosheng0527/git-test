558f6c888cd0ce0a63fba763d7f4a5ff9248c052
youxiaocheng <2412347023@qq.com>
2021-08-28 12:45:03 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author shengxiao
+ * @date 2021/8/28 12:42
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
